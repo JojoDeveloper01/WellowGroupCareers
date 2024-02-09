4c1e726c76d267b78532a2aa561eaaf15eb33fe9
Guilherme2024 <guilherme.carvalho@wellowgroup.com>
2024-02-09 12:08:48 +0000

    formulario not ready

--- src/components/module/formulario.jsx
+++ src/components/module/formulario.jsx
@@ -1,14 +1,21 @@
+
 "use client"
+import './formulario.css';
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Form = ({ isOpen, onClose }) => {
-  useEffect(() => {
-    document.body.classList.add('no-scroll'); // Não há scroll 
-    return () => {
-      document.body.classList.remove('no-scroll'); // Há scroll 
-    };
-  }, []);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const totalPages = 3; // Defina o número total de páginas/secções
 
   const formVariants = {
     initial: {
@@ -34,7 +41,7 @@ const Form = ({ isOpen, onClose }) => {
     <AnimatePresence>
       {isOpen && (
         <motion.section
-          className="bg-[#00000057] translate-y-0 translate-x-0 fade flex items-center justify-center w-screen h-screen fixed inset-0 z-[2]"
+          className="bg-[#00000057] translate-y-0 translate-x-0 fade flex items-center justify-center w-screen h-screen fixed inset-0 z-[2] "
           initial="initial"
           animate="animate"
           exit="exit"
@@ -43,15 +50,189 @@ const Form = ({ isOpen, onClose }) => {
           <motion.div
             key="unique-key"
             onClick={(e) => e.stopPropagation()}
-            className="module-form w-[31vw] h-[72vh] bg-white"
+            className="modal-body"
             variants={formVariants}
           >
-            {/* Seu conteúdo do formulário aqui */}
+
+              {currentPage === 1 && (
+                <div id="reservar">
+                  {/* Conteúdo da primeira página/secção (Dados Pessoais) */}
+
+                  <div className='barNext-level'>
+                    <div className='barLevel'>
+                      <div className='round level_here'>1</div>
+                      <h2>Dados Pessoais</h2>
+                      <div className='bar'></div>
+                    </div>
+
+                    <div className='barLevel'>
+                      <div className='round'>2</div>
+                      <h2>Qualificações</h2>
+                      <div className='bar'></div>
+                    </div>
+
+                    <div className='barLevel'>
+                      <div className='round'>3</div>
+                    </div>
+                  </div>
+
+                  <section className='write-section'>
+                    <h2>Nome<span className='req'> *</span></h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Apelido</h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Contacto</h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Email<span className='req'> *</span></h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Linkedin</h2>
+                    <input></input>
+                  </section>
+                </div>
+              )}
+              {currentPage === 2 && (
+                <div id="reservar">
+                  {/* Conteúdo da segunda página/secção (Qualificações) */}
+
+                  <div className='barNext-level'>
+                    <div className='barLevel'>
+                      <div className='round'>1</div>
+                      <div className='bar min'></div>
+                    </div>
+
+                    <div className='barLevel'>
+                      <div className='round level_here'>2</div>
+                      <h2>Qualificações</h2>
+                      <div className='bar min'></div>
+                    </div>
+
+                    <div className='barLevel'>
+                      <div className='round'>3</div>
+                      <h2>Linguagens e Mobilidade</h2>
+                    </div>
+                  </div>
+
+                  <section className='write-section title-section'>
+                    <h2>Formação Acadêmica</h2>
+                  </section>
+
+
+                  <section className='write-section'>
+                    <h2>Nome Instituição</h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Designação de Curso</h2>
+                    <input></input>
+                  </section>
+
+
+                  <section className='write-section title-section'>
+                    <h2>Experiência Professional Relevante</h2>
+                  </section>
+
+
+                  <section className='write-section'>
+                    <h2>Nome Empresa</h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Função</h2>
+                    <input></input>
+                  </section>
+
+                </div>
+              )}
+              {currentPage === 3 && (
+                <div id="reservar">
+                  {/* Conteúdo da terceira página/secção (Linguagem e Mobilidade) */}
+
+                  <div className='barNext-level'>
+                    <div className='barLevel'>
+                      <div className='round'>1</div>
+                      <div className='bar min'></div>
+                    </div>
+
+                    <div className='barLevel'>
+                      <div className='round'>2</div>
+                      <h2>Qualificações</h2>
+                      <div className='bar min'></div>
+                    </div>
+
+                    <div className='barLevel'>
+                      <div className='round level_here'>3</div>
+                      <h2>Linguagens e Mobilidade</h2>
+                    </div>
+                  </div>
+
+                  <section className='write-section'>
+                    <h2>Nome<span className='req'> *</span></h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Apelido</h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Contacto</h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Email<span className='req'> *</span></h2>
+                    <input></input>
+                  </section>
+
+                  <section className='write-section'>
+                    <h2>Linkedin</h2>
+                    <input></input>
+                  </section>
+                </div>
+              )}
+
+              {/* Botões de navegação entre as páginas/secções */}
+              <div className="button-section flex">
+                {currentPage == 1 && (
+                  <div>
+                    <button className="button" disabled onClick={handlePreviousPage}>Anterior</button>
+                    <button className="button" onClick={handleNextPage}>Seguinte</button>
+                  </div> 
+                )}
+                {currentPage == 2 && (
+                  <div>
+                    <button className="button" onClick={handlePreviousPage}>Anterior</button>
+                    <button className="button" onClick={handleNextPage}>Seguinte</button>
+                  </div> 
+                )}
+                {currentPage == 3 && (
+                  <div>
+                    <button className="button" onClick={handlePreviousPage}>Anterior</button>
+                    <button className="button">Enviar</button>
+                  </div> 
+                )}
+
+              </div>
           </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
 export default Form;
