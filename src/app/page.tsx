e44a3df5a9ed1cf592adc4c05533dce967c75229
nunofranciscowellow <nuno.francisco@wellowgroup.com>
2024-02-09 11:50:22 +0000

    buttonOffer not showing error solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-import OfferCard from '@/components/offerCard/offerCard';
-import SearchBar from '@/components/searchBar/searchBar';
+import SearchBar from "@/components/searchBar/searchBar";
+import OfferCard_Array from "@/components/offerCard/offerCard";
 import "./page.css";
 
 /* dfsfdsdfsdfsdfsdfsdfHFGDHFGDHFGHFGH */
 
 export default function Home() {
-  return (
-
-    <div className="container">
-      <SearchBar />
-      <section className="section-offers">
-        <h2 className="section-title">Oportunidades</h2>
-        <OfferCard />
-      </section>
-
-    </div>
-  );
+	return (
+		<div className='container'>
+			<SearchBar />
+			<section className='section-offers'>
+				<h2 className='section-title'>Oportunidades</h2>
+				<OfferCard_Array />
+			</section>
+		</div>
+	);
 }
--- src/components/ButtonOffer/buttonOffer.jsx
+++ src/components/ButtonOffer/buttonOffer.jsx
@@ -1,23 +1,33 @@
-'use client'
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
 
 export default function buttonOffer({ job }) {
-  /*   const router = useRouter()
+	/*   const router = useRouter()
     function handleClick() {
       router.push(`/${id}`)
     } */
 
-  /*  const filteredOffers = id.filter((job) => job.title === title && job.custom_status !== 0); */
+	/*  const filteredOffers = id.filter((job) => job.title === title && job.custom_status !== 0); */
 
-  /*   const URL = `/${encodeURI(filteredOffers[0].internal_job_title.replace(/-{2,}/g, "-").replace(/\s+/g, "").replace(/\//g, "-").replace(/[A-Z]/g, (match) => match.toLowerCase()))}`
-   */
-  return (
-    job.length > 0 && (
-      <Link key={job[0].id} href={`/${encodeURI(job[0].internal_job_title.replace(/-{2,}/g, "-").replace(/\s+/g, "").replace(/\//g, "-").replace(/[A-Z]/g, (match) => match.toLowerCase()))}`}>
-        <button className="mr-2 btn-secondary" style={{ height: '45px' }}>
-          Ver Oferta
-        </button>
-      </Link>
-    )
-  )
+	/*   const URL = `/${encodeURI(filteredOffers[0].internal_job_title.replace(/-{2,}/g, "-").replace(/\s+/g, "").replace(/\//g, "-").replace(/[A-Z]/g, (match) => match.toLowerCase()))}`
+	 */
+
+	return (
+		job && (
+			<Link
+				key={job.id}
+				href={`/${encodeURI(
+					job.internal_job_title
+						.replace(/-{2,}/g, "-")
+						.replace(/\s+/g, "")
+						.replace(/\//g, "-")
+						.replace(/[A-Z]/g, match => match.toLowerCase())
+				)}`}
+			>
+				<button className='mr-2 btn-secondary' style={{ height: "45px" }}>
+					Ver Oferta
+				</button>
+			</Link>
+		)
+	);
 }
